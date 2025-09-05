5b14c80535541925e8063d8e03f1b25021d2ecb4
Yiuko <cibickantoine@gmail.com>
2025-09-05 18:05:57 +0200

    azeae

--- src/app/products/screen/page.tsx
+++ src/app/products/screen/page.tsx
@@ -32,22 +32,25 @@ backgroundImage="SCREEN.png"
 />
 
 
-<section className="py-20">
-        <div className="container flex justify-center">
+<section className="py-20 relative">
+        <div className="container">
+          <div className="flex justify-center relative">
           <div 
-            className="p-1 rounded-3xl"
+            className="p-1 rounded-3xl relative z-10"
             style={{ 
               background: 'linear-gradient(45deg, #473FB9, #4DA8D7, #9512B6)',
               width: '1441.43px', 
               height: '709.46px' 
             }}
           >
+          <h2 className="text-[40px] font-medium text-white text-center uppercase absolute top-40 left-0 right-0 z-20">LED SCREEN BY VISUAAL</h2>
             <div className="w-full h-full rounded-3xl overflow-hidden" style={{ backgroundColor: '#211824' }}>
               <script type="module" src="https://unpkg.com/@splinetool/viewer@1.10.51/build/spline-viewer.js"></script>
               <spline-viewer url="https://prod.spline.design/qDj32pWs0uTcm5kM/scene.splinecode" style={{ width: '100%', height: '100%' }}></spline-viewer>
             </div>
           </div>
-        </div>
+          </div>
+        </div> 
       </section>
 
       
